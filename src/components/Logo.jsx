bf7e22a1643b84604f2e203a3a0a090ebcb8c3b9
sparshsinghal885 <sparshsinghal885@gmail.com>
2024-07-20 17:18:34 +0530

    logo issue resolved

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Logo({ width = '100px' }) {
   return (
     <div className={`w-[${width}]`}>
-      <img src="https://cdn.pixabay.com/photo/2017/01/31/23/42/animal-2028258_1280.png" alt="logo" />
+      <img src="https://cdn.pixabay.com/photo/2017/01/31/23/42/animal-2028258_1280.png" alt="logo"
+        className='w-[100px]'
+      />
     </div>
   )
 }
